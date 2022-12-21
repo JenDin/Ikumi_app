899f52d4fdd54ea204451e2f8732117234cae9c8
Jenzi <jennydinh94@gmail.com>
2022-12-21 13:25:44 +0100

    Create food menu page and button component

---
+++ Ikumi_app/src/components/Button.vue
@@ -0,0 +1,23 @@
+<!-- HTML code -->
+<template>
+  <button
+    class="p-2 uppercase font-semibold border-black border-2 bg-black text-white w-full hover:border-2 hover:border-orange-400 hover:bg-orange-400 hover:text-black"
+  >
+    {{ btnText }}
+  </button>
+</template>
+
+<!-- JavaScript code -->
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+  name: "Button",
+  props: {
+    btnText: String,
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped></style>
---
+++ Ikumi_app/src/components/SashimiMenu.vue
@@ -0,0 +1,29 @@
+<!-- HTML code -->
+<template>
+  <div
+    class="container max-w-md text-center mb-2"
+    v-if="dish.category === 'Sashimi/Nigiri'"
+  >
+    <div class="flex flex-wrap justify-between">
+      <p class="font-semibold">{{ dish.title }}</p>
+
+      <p class="text-sm">¥{{ dish.price }}</p>
+    </div>
+
+    <div class="flex justify-start">
+      <p class="text-sm">{{ dish.description }}</p>
+    </div>
+  </div>
+</template>
+
+<!-- JavaScript code -->
+<script>
+export default {
+  props: {
+    dish: Object,
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped></style>
