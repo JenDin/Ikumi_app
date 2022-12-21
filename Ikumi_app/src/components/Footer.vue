f739efd180b3c70229d7d15cf3c24c0e5aba8826
Jenzi <jennydinh94@gmail.com>
2022-12-21 13:21:45 +0100

    Create footer

---
+++ Ikumi_app/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<!-- HTML code -->
+<template>
+  <footer
+    class="bg-[#131515be] text-white p-4 fixed bottom-0 w-full flex justify-between items-center"
+  >
+    <div>
+      <h3 class="">Ikumi</h3>
+      <p class="">267-1154, Shibasaki</p>
+      <p class="">Tokyo, Japan</p>
+    </div>
+    <div class="text-2xl">
+      <a :href="facebookLink" target="_blank"
+        ><i class="fa-brands fa-square-facebook px-4 hover:text-[1.8rem]"></i
+      ></a>
+      <a :href="instagramLink" target="_blank"
+        ><i class="fa-brands fa-instagram hover:text-[1.8rem]"></i
+      ></a>
+    </div>
+  </footer>
+</template>
+
+<!-- JavaScript code -->
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      facebookLink: "https://www.facebook.com/",
+      instagramLink: "https://www.instagram.com/",
+    };
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped></style>
