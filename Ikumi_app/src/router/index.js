925d0fa9229483f6e2b4daf7b5e971ba8d82d666
Jenzi <jennydinh94@gmail.com>
2022-12-21 13:35:51 +0100

    Create gallery and add photos

--- Ikumi_app/src/router/index.js
+++ Ikumi_app/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import FoodMenuView from "../views/FoodMenuView.vue";
 import DrinkMenuView from "../views/DrinkMenuView.vue";
+import GalleryView from "../views/GalleryView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/drinkmenu",
       name: "drink-menu",
       component: DrinkMenuView,
+    },
+    {
+      path: "/gallery",
+      name: "gallery",
+      component: GalleryView,
     },
   ],
 });
---
+++ Ikumi_app/src/views/GalleryView.vue
@@ -0,0 +1,63 @@
+<!-- HTML code -->
+<template>
+  <Header />
+  <h1 class="text-4xl text-center uppercase font-bold mt-8 mb-4">Gallery</h1>
+
+  <div class="container mx-auto px-4">
+    <section class="py-8 px-4">
+      <div class="flex flex-wrap -mx-4">
+        <div class="hidden md:block md:w-1/2 px-4">
+          <div
+            class="h-full w-full bg-cover hover:opacity-80"
+            :style="{ 'background-image': tunaSushi }"
+          ></div>
+        </div>
+        <div class="md:w-1/2 h-auto px-4">
+          <div class="mb-8 hover:opacity-80">
+            <img :src="sushiChef" alt="" />
+          </div>
+          <div class="hover:opacity-80">
+            <img :src="shrimpSushi" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pb-36 px-4">
+      <div class="flex flex-wrap -mx-4">
+        <div class="md:w-1/2 px-4 mb-8 md:mb-0 hover:opacity-80">
+          <img :src="sashimiPlate" alt="" />
+        </div>
+        <div class="md:w-1/2 px-4 mb-8 md:mb-0 hover:opacity-80">
+          <img :src="sashimiPlate2" alt="" />
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <Footer />
+</template>
+
+<!-- JavaScript code -->
+<script>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      tunaSushi: "url('src/assets/single_sushi.jpg')",
+      sushiChef: `src/assets/sushi_chef.jpg`,
+      shrimpSushi: `src/assets/sushi_shrimp.jpg`,
+      sashimiPlate: `src/assets/sashimi_plate2.png`,
+      sashimiPlate2: `src/assets/sashimi_plate.png`,
+    };
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped></style>
