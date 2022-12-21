9e378d7f3cb762fb983dee2ca47ee89f768ece58
Jenzi <jennydinh94@gmail.com>
2022-12-21 13:20:23 +0100

    Create header component

---
+++ Ikumi_app/src/components/Header.vue
@@ -0,0 +1,130 @@
+<!-- HTML code -->
+<template>
+  <header>
+    <nav
+      class="navbar flex justify-between items-center py-2 px-4 min-h-[70px] bg-[#131515be] text-white"
+    >
+      <div href="#">
+        <RouterLink to="/" class="text-2xl uppercase text-white">
+          <h1 class="text-white">Ikumi</h1>
+        </RouterLink>
+      </div>
+      <!-- Nav links -->
+      <ul
+        class="navMenu flex justify-between items-center gap-8 pb-2 md:pb-0"
+        :class="[{ active: isActive }]"
+      >
+        <li class="my-3 uppercase hover:text-orange-400">
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li class="my-3 uppercase hover:text-orange-400">
+          <RouterLink to="/menu">Menu</RouterLink>
+        </li>
+        <li class="my-3 uppercase hover:text-orange-400">
+          <RouterLink to="/gallery">Gallery</RouterLink>
+        </li>
+        <li
+          class="my-3 p-1.5 uppercase border-solid border-2 hover:border-orange-400"
+        >
+          <RouterLink to="/reservations">Reservations</RouterLink>
+        </li>
+      </ul>
+
+      <!-- Hamburger menu -->
+      <div
+        @click="toggleHamburgerMenu()"
+        class="cursor-pointer md:hidden"
+        :class="['hamburger', { active: isActive }]"
+      >
+        <span
+          class="bg-white transition ease-in-out delay-50 bar block w-7 h-0.5 rounded my-1.5"
+        ></span>
+        <span
+          class="bg-white transition ease-in-out delay-50 bar block w-7 h-0.5 rounded my-1.5"
+        ></span>
+        <span
+          class="bg-white transition ease-in-out delay-50 bar block w-7 h-0.5 rounded my-1.5"
+        ></span>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<!-- JavaScript code -->
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+  data() {
+    return {
+      isActive: false,
+      landingPage: true,
+    };
+  },
+  name: "Header",
+  methods: {
+    toggleHamburgerMenu() {
+      // Toggle the hamburger menu
+      this.isActive = !this.isActive;
+    },
+    onResize() {
+      // Close the hamburger menu on bigger screens
+      if (window.innerWidth < 768) {
+        this.isActive = !this.isActive;
+      } else {
+        return this.isActive;
+      }
+    },
+    mouseOver() {
+      const reservations = document.querySelector("#test");
+      reservations.style.textDecoration = "underline";
+      reservations.style.textDecorationColor = "#fb923c";
+      reservations.style.textDecorationThickness = "3px";
+    },
+    mouseLeave() {
+      const reservations = document.querySelector("#test");
+      reservations.style.textDecoration = "none";
+    },
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped>
+@media screen and (min-width: 300px) and (max-width: 768px) {
+  /* Create an 'X' once the hamburger menu has been clicked */
+  .hamburger.active .bar:nth-child(2) {
+    opacity: 0;
+  }
+  .hamburger.active .bar:nth-child(1) {
+    transform: translateY(10px) rotate(45deg);
+  }
+  .hamburger.active .bar:nth-child(3) {
+    transform: translateY(-6px) rotate(-45deg);
+  }
+
+  .navMenu {
+    position: fixed;
+    left: -100%;
+    top: 70px;
+    gap: 0;
+    flex-direction: column;
+    width: 100%;
+    text-align: center;
+    transition: 0.3s;
+    background: #131515be;
+  }
+
+  .nav-item {
+    margin: 16px 0;
+  }
+
+  .active {
+    left: 0;
+    z-index: 1;
+  }
+  .deactive {
+    left: 100;
+  }
+}
+</style>
--- Ikumi_app/src/views/HomeView.vue
+++ Ikumi_app/src/views/HomeView.vue
@@ -1,10 +1,46 @@
 <!-- HTML code -->
-<template></template>
+<template>
+  <div class="hero-image" :style="{ backgroundImage }">
+    <Header />
+    <h2 class="p-3 pl-5 absolute bottom-10 text-white">IKUMI</h2>
+    <p class="p-3 pl-5 absolute bottom-5 text-white">267-1154, Shibasaki</p>
+    <p class="p-3 pl-5 absolute bottom-0 text-white">Tokyo, Japan</p>
+  </div>
+</template>
 
 <!-- JavaScript code -->
 <script>
-export default {};
+import Header from "../components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      backgroundImage: "url('src/assets/sushi_bg.jpg')",
+    };
+  },
+};
 </script>
 
 <!-- CSS styles -->
-<style scoped></style>
+<style scoped>
+.hero-image {
+  width: 100%;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  z-index: 0;
+}
+
+@media screen and (min-width: 300px) and (max-width: 768px) {
+  .hero-image {
+    /* width: 100%;
+	  height: 100vh; */
+    background-position: center;
+    /* background-size: cover; */
+  }
+}
+</style>
