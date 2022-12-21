817555ec5b8efe87e9a2f489181086365e3e0288
Jenzi <jennydinh94@gmail.com>
2022-12-21 13:27:28 +0100

    Create drink menu

--- Ikumi_app/src/router/index.js
+++ Ikumi_app/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import FoodMenuView from "../views/FoodMenuView.vue";
+import DrinkMenuView from "../views/DrinkMenuView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/menu",
       name: "food-menu",
       component: FoodMenuView,
+    },
+    {
+      path: "/drinkmenu",
+      name: "drink-menu",
+      component: DrinkMenuView,
     },
   ],
 });
---
+++ Ikumi_app/src/views/DrinkMenuView.vue
@@ -0,0 +1,89 @@
+<!-- HTML code -->
+<template>
+  <Header />
+  <h1 class="text-4xl text-center uppercase font-bold mt-8 mb-10">Drinks</h1>
+
+  <div class="flex justify-center">
+    <button
+      class="mb-8 border-2 border-black p-2 hover:bg-black hover:text-white"
+    >
+      <RouterLink to="/menu">Sushi menu</RouterLink>
+    </button>
+  </div>
+
+  <div class="lg:flex md:w-10/12 lg:w-1/2 m-auto max-w-6xl">
+    <article class="p-4 mt-10">
+      <h2 class="text-2xl text-center uppercase font-bold">Wine</h2>
+      <div class="flex justify-center items-center max-w-xl m-auto flex-wrap">
+        <DrinkMenu v-for="drink in drinks" :drink="drink" :key="drink.id" />
+      </div>
+    </article>
+
+    <div
+      class="max-w-[90%] m-auto lg:m-0 lg:max-w-full border-2 h-auto relative top-0 border-black"
+    ></div>
+
+    <article class="p-4 mt-10">
+      <h2 class="text-2xl text-center uppercase font-bold">Saké</h2>
+      <div class="flex justify-center items-center max-w-xl m-auto flex-wrap">
+        <DrinkMenu v-for="drink in drinks" :drink="drink" :key="drink.id" />
+      </div>
+    </article>
+  </div>
+
+  <Footer />
+  <!-- </div> -->
+</template>
+
+<!-- JavaScript code -->
+<script>
+import Header from "../components/Header.vue";
+import Button from "../components/Button.vue";
+import DrinkMenu from "../components/DrinkMenu.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Button,
+    DrinkMenu,
+    Footer,
+  },
+  data() {
+    return {
+      drinks: [],
+    };
+  },
+  methods: {
+    async getDishes() {
+      try {
+        const resp = await fetch("http://localhost:3000/api/drinks");
+
+        const data = await resp.json();
+
+        this.dishes = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getDrinks() {
+      try {
+        const resp = await fetch("http://localhost:3000/api/drinks");
+
+        const data = await resp.json();
+
+        this.drinks = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getDishes();
+    this.getDrinks();
+  },
+};
+</script>
+
+<!-- CSS styles -->
+<style scoped></style>
