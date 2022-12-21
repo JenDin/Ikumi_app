9943fec0b342eecd80630f0f4d4ce9a422311928
Jenzi <jennydinh94@gmail.com>
2022-12-21 13:17:53 +0100

    First commit

---
+++ Ikumi_app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <main>
+    <h1></h1>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
--- README.md
+++
@@ -1,1 +0,0 @@
-# Ikumi_app
